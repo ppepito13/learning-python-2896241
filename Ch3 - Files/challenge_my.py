39710b5ea68d1083bd04be3402c2fa821fc2ed86
pepito <adres_smieciowy@outlook.com>
2022-08-31 14:13:50 +0200

    Challenge2 complete

--- Ch3 - Files/challenge_my.py
+++ Ch3 - Files/challenge_my.py
@@ -4,7 +4,6 @@
 import os
 from os import path
 
-
 # create dir and open file
 results_dir = "results"
 os.makedirs(results_dir, exist_ok=True)
@@ -13,7 +12,7 @@ src = path.join(results_dir, "results.txt")
 # get the size and list of files
 files = ""
 total_size = 0
-list_files = os.listdir(os.getcwd())
+list_files = os.listdir()
 for i in list_files:
 	if path.isfile(i):
 		total_size = total_size + os.stat(i).st_size
@@ -23,9 +22,9 @@ for i in list_files:
 # open file and append results
 def write_results():
 	my_file = open(src, "w+")
-	my_file.write("Total size in bytes: " + str(total_size) + "\n")
-	my_file.write("Files list:\n")
-	my_file.write("-------------")
+	my_file.write("Total size in bytes: " + str(total_size) + "\n" +
+	              "Files list:\n" +
+	              "-------------")
 	my_file.write(files)
 	my_file.close()
 
